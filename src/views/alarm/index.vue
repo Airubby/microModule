48aca72b6b0a4cb63f7feb11bdb92495e949ad4b
Airubby <ai_rubby@qq.com>
2020-03-23 11:24:55 +0800

    告警界面

--- src/views/alarm/index.vue
+++ src/views/alarm/index.vue
@@ -4,20 +4,8 @@
         <!-- 如果需要分页器 -->
         <div class="swiper-pagination"></div>
         <div class="swiper-wrapper" id="swiper-wrapper">
-            <div class="swiper-slide">
-                <info-alarm></info-alarm>
-            </div>
-            <div class="swiper-slide">
-                <info-alarm></info-alarm>
-            </div>
-            <div class="swiper-slide">
-                <info-alarm></info-alarm>
-            </div>
-            <div class="swiper-slide">
-                <info-alarm></info-alarm>
-            </div>
-            <div class="swiper-slide">
-                <info-alarm></info-alarm>
+            <div class="swiper-slide" v-for="(item,index) in arr">
+                <component :is="item.component" :data="item"></component>
             </div>
         </div>
     </div>
@@ -25,34 +13,28 @@
 </template>
 
 <script>
+import swiper from '@/views/public/mixin/swiper'
 import infoAlarm from './infoAlarm.vue'
 export default {
+    mixins:[swiper],
+    components: {infoAlarm},
+    created () {
+        let arr=[
+            {key:"AlarmDetail",component:"infoAlarm"},
+        ]
+        this.arr=arr;
+    },
     mounted() {
-        let _this=this;
-        new this.$Swiper('#swiper-container', {
-            autoplay: 0,
-            spaceBetween: 0,
-            slidesPerView: 1,
-            speed: 500,
-            effect: 'slide',
-            pagination: '.swiper-pagination',
-            paginationClickable: true,
-            paginationBulletRender: function (swiper, index, className) {
-                return '<span class="' + className + ' slide'+index+'">' + _this.title[index] + '</span>';
-            }
-        })
+        
     },
     data(){
         return{
-            title:["所有告警","紧急告警","重要告警","一般告警","提示"]
+            
         }
     },
     methods: {
         
     },
-    components: {
-        infoAlarm
-    }
 }
 </script>
 <style lang="less" scoped>
--- src/views/alarm/infoAlarm.vue
+++ src/views/alarm/infoAlarm.vue
@@ -1,29 +1,92 @@
 <template>
     <div class="content">
-        <div class="table-info">
-            <div class="table-info-title">
-                <div class="table-info-box">对象</div>
-                <div class="table-info-box">告警名称</div>
-                <div class="table-info-box table-info-box1">告警描述</div>
-                <div class="table-info-box">产生时间</div>
-                <div class="table-info-box">确认时间</div>
-                <div class="table-info-box">操作列</div>
-            </div>
-            <div class="table-info-con">
-                <el-scrollbar style="height:100%;" class="scrollbar">
-                    <div class="scrollbarbox">
-                        <div class="table-info-list" v-for="item in 30">
-                            <div class="table-info-box">025252455</div>
-                            <div class="table-info-box">撒的发了</div>
-                            <div class="table-info-box table-info-box1">开门酸辣粉就撒了发撒大家</div>
-                            <div class="table-info-box">2019-04-25 11:22:32</div>
-                            <div class="table-info-box">2019-04-25 11:22:32</div>
-                            <div class="table-info-box"></div>
-                        </div>
-                    </div>
-                </el-scrollbar>
-            </div>
+        <div class="table_search" :class="{'active':searchHiden}">
+            <el-form ref="ValidateForm" class="form-serarch" :model="initParams" label-width="75px">
+                <el-row :gutter="0">
+                    <el-col :span="10">
+                        <el-form-item label="发生时间">
+                            <el-date-picker
+                            v-model="initParams.time"
+                            type="daterange"
+                            range-separator="至"
+                            start-placeholder="开始日期"
+                            end-placeholder="结束日期">
+                            </el-date-picker>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="14">
+                        <el-form-item label="">
+                            <span @click="changeSearch" class="changeBtn">{{changeInfo}}<i :class="[searchHiden?'el-icon-arrow-up':'el-icon-arrow-down']"></i></span>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-col :span="9">
+                            <el-form-item label="事件来源">
+                                <div style="width:calc(100% - 80px);float:left;">
+                                <el-input v-model="initParams.code" readonly>
+                                    <template slot="append"><span @click="selectPoint">{{$t("Select")}}</span></template>
+                                </el-input>
+                                </div>
+                                <span @click="changeSearch" class="color ml5">清空</span>
+                            </el-form-item>
+                        </el-col>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-col :span="9">
+                            <el-form-item label="告警等级">
+                                <el-checkbox-group v-model="initParams.checkboxGroup">
+                                    <el-checkbox label="5" border><img src="/images/alarm-5.svg" class="alarm-img">紧急</el-checkbox>
+                                    <el-checkbox label="4" border><img src="/images/alarm-4.svg" class="alarm-img">严重</el-checkbox>
+                                    <el-checkbox label="3" border><img src="/images/alarm-3.svg" class="alarm-img">重要</el-checkbox>
+                                    <el-checkbox label="2" border><img src="/images/alarm-2.svg" class="alarm-img">一般</el-checkbox>
+                                    <el-checkbox label="1" border><img src="/images/alarm-1.svg" class="alarm-img">提示</el-checkbox>
+                                </el-checkbox-group>
+                            </el-form-item>
+                        </el-col>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-col :span="9">
+                            <el-form-item label="事件状态">
+                                <el-checkbox-group v-model="initParams.checkboxGroup">
+                                    <el-checkbox label="5" border>已恢复</el-checkbox>
+                                    <el-checkbox label="4" border>未恢复</el-checkbox>
+                                </el-checkbox-group>
+                            </el-form-item>
+                        </el-col>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-col :span="9">
+                            <el-form-item label="确认状态">
+                                <el-checkbox-group v-model="initParams.checkboxGroup">
+                                    <el-checkbox label="5" border>已确认</el-checkbox>
+                                    <el-checkbox label="4" border>未确认</el-checkbox>
+                                </el-checkbox-group>
+                            </el-form-item>
+                        </el-col>
+                    </el-col>
+                </el-row>
+            </el-form>
         </div>
+        <el-table-pagination
+            :url="$ajaxUrl+'/getTable'"
+            list-field="data" 
+            total-field="total"
+            method='get' 
+            type="local"
+            :data="table_data"
+            :params="initParams"
+            :showPagination="true"
+            :showSelectAll="true"
+            :columns="table_columns" ref="thisRef">   
+            <el-table-column slot="prepend" type="selection"></el-table-column>
+            <template slot-scope="scope" slot="preview-name">
+                <span>{{scope.row.code}}</span>
+            </template>
+            <template slot-scope="scope" slot="preview-status">
+                <span v-if="scope.row.status=='1'">{{$t("Inservice")}}</span>
+                <span v-else class="alarm">{{$t("NotRun")}}</span>
+            </template>
+        </el-table-pagination>
     </div>
 </template>
 
@@ -34,24 +97,45 @@ export default {
         
     },
     mounted() {
-        let _this=this;
-        let scrollbar=this.$el.querySelector(".el-scrollbar__wrap");
-        scrollbar.onscroll=function(){
-            let viewH=scrollbar.offsetHeight;
-            let contentH=_this.$el.querySelector(".scrollbarbox").offsetHeight;
-            let scrollTop=scrollbar.scrollTop;
-            if((scrollTop + viewH) >= contentH){  
-                _this.$message.success('请求分页加载更多数据了~~~');
-            }
-        }
+        
     },
     data(){
         return{
-            
+            searchHiden:false,
+            changeInfo:'展开筛选',
+            initParams:{
+                code:'',
+                type:'',
+                user:'',
+                indate:'',
+                timegroup:'',
+                checkboxGroup:[]
+            },
+            table_columns:[
+                { prop: 'code', label: '告警等级',slotName:'preview-name',minWidth:10},
+                { prop: 'type', label: "事件来源",minWidth:10},
+                { prop: 'status', label: "事件内容",slotName:'preview-status',minWidth:10},
+                { prop: 'user', label: "发生时间",minWidth:10},
+                { prop: 'indate', label: "恢复时间",minWidth:20},
+                { prop: 'indate', label: "确认人",minWidth:20},
+                { prop: 'indate', label: "确认时间",minWidth:20},
+                { prop: 'indate', label: "确认内容",minWidth:20},
+                { prop: 'indate', label: "出来建议",minWidth:20},
+            ],
+            table_data:[
+                {"code":"dslf","type":"dslf","user":"dslf","indate":"dslf","status":"1","d":"dslf"},
+                {"code":"dslf","type":"dslf","user":"dslf","indate":"dslf","status":"0","d":"dslf"},
+            ],
         }
     },
     methods: {
-      
+        changeSearch:function(){
+            this.changeInfo=this.searchHiden?'展开筛选':'收起筛选';
+            this.searchHiden=!this.searchHiden;
+        },
+        selectPoint:function(){
+
+        }
     },
     components: {
       
@@ -59,5 +143,27 @@ export default {
 }
 </script>
 <style scoped lang="less">
-    
+    .table_search{
+        max-height: 50px;
+        overflow: hidden;
+        transition: all 0.4s ease-in;
+        &.active{
+            max-height: 500px;
+        }
+    }
+    .alarm-img{
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        vertical-align: bottom;
+        margin-right: 4px;
+    }
+    .changeBtn{
+        background: #E5E6E9;
+        height: 32px;
+        line-height: 32px;
+        display: inline-block;
+        padding: 0px 12px;
+        border-radius: 20px;
+    }
 </style>
